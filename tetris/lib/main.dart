00cf062954f3dd1cd0b9fda9cb2544b2a2e130e8
Johana Jaramillo <81990862+JJ-OO@users.noreply.github.com>
2023-11-11 16:32:45 -0500

    material code added

--- tetris/lib/main.dart
+++ tetris/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'board.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,19 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameBoard(),
+    );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-    }
